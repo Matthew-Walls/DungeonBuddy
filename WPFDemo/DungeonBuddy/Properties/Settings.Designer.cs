ec360f7fd47213694f176733578225595d5322ee
Matthew Walls <matthewwalls94@outlook.com>
2019-01-19 23:19:16 -0500

    Replaced old .mdf file with new one from github, deleted old data sets and re added theme using the new database

--- WPFDemo/DungeonBuddy/Properties/Settings.Designer.cs
+++ WPFDemo/DungeonBuddy/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DungeonBuddy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace DungeonBuddy.Properties {
         public string DungeonBuddyConnectionString {
             get {
                 return ((string)(this["DungeonBuddyConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Git\\DungeonBuddy-local\\WPF" +
+            "Demo\\Resources\\DungeonBuddy.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string DungeonBuddyDBConnection {
+            get {
+                return ((string)(this["DungeonBuddyDBConnection"]));
             }
         }
     }
